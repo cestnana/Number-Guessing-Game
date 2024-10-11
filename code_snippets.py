355143419d199829a27344486cb466cef236c9e3
Remi Sha <105662433+cestnana@users.noreply.github.com>
2024-10-12 01:47:11 +0800

    add code snippets for backup usage

---
+++ code_snippets.py
@@ -0,0 +1,13 @@
+# test print random integer: 
+# print(target_number)
+# for _ in range(10):
+#   print(random.randint(1, 100))
+
+# main game logic
+# if attempt_left <= 0:
+#   print("'result':, you lose.")
+# else:
+  
+# display the target number for test
+# print(f"The target number is: {target_number}")
+
