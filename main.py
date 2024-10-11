4ea9bd68e0e2c692b9414c57b029932369a75a68
Remi Sha <105662433+cestnana@users.noreply.github.com>
2024-10-12 02:51:21 +0800

    pruned message display by combining lines

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
 # import external module/file: 
-import random
-# import logo file
-import art
+from random import randint
+# import logo from module/file:
+from art import logo
 
+# ------------------
 # global variables:
 
-# test print random integer: 
-
-# print(target_number)
-# for _ in range(10):
-#   print(random.randint(1, 100))
-
 # generate a random integer number between 1 to 100
-target_number = random.randint(1, 100)
+target_number = randint(1, 100)
 
 # initialize user guessing number
 guess_number = 0
@@ -35,19 +30,22 @@ game_mode = "not choose yet"
 # flag/result of checking user input is number
 is_number = True
 
+# ----------------------
 # function declarations: 
 
 # display current status
 def current_status(): 
-  print(f"You have {attempt_left} attempts remaining to guess the number.")
+  print(f"You have {attempt_left}/{total_attempts} attempts remaining to guess the number.")
   
-# Input Validation: 
-def check_is_number(value):
-  return isinstance(value, (int, float))
+# display initial status
+def initial_status(): 
+  print(f"You have {total_attempts} attempts to guess the number.")
+  
 
+# -----------
 # main logic: 
 
-print(art.logo)
+print(logo)
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a whole number between 1 and 100.")
 game_mode = input("Choose a difficulty. Type 'e' for easy or 'h' for 'hard': ")
@@ -57,81 +55,51 @@ if game_mode == "h":
   total_attempts = 5
 else:
   total_attempts = 10
-  
+
 # set attempts left
 attempt_left = total_attempts - attempt_count
 
-# main game logic
-# if attempt_left <= 0:
-#   print("'result':, you lose.")
-# else:
-  
-# display the target number for test
-# print(f"The target number is: {target_number}")
+# display the initial status
+initial_status()
 
-while attempt_left > 0:
-  # display the target number for test
-  # print(f"The target number is: {target_number}")
-  
-  # check if user is not first guessing
-  if not is_first_attempt:
-    print("Guess again.")
-  
-  # display current status
-  current_status()
-  
-  # check user input is number
+# if user still has attempts left
+while attempt_left > 0:  
+  # get user input and validate it
   while True:
-    # user_input = input("Please enter a number:")
-    
     # get user's guessing number
     guess_number = (input("Make a guess: "))
-
+    # check user input is number
     try:
       value = int(guess_number)
-      print("The value you entered is:", value)
+      # print("The value you entered is:", value)
       break  # Input correctly will exit the loop
     except ValueError:
       # catch the error and handle it
       print("What you entered is not a whole number. Please re-enter it. (e.g. 51)")
         
-  # # get user's guessing number
-  # guess_number = int(input("Make a guess: "))
-  # ! this may cause error when user input a really str
-  # ! how to handle this ?
-  
-  # check user input is number
-  # is_number = check_is_number(guess_number)
-  
-  # *input validation*
-  # while is_number == False:
-  #   print("Please type a number. (e.g. 50)")
-  #   guess_number = input("Make a guess: ")
-  #   is_number = check_is_number(guess_number)
-  #   if is_number == True:
-  #     break
-  
   # user attempts + 1
   attempt_count += 1
   # update attempts left
   attempt_left = total_attempts - attempt_count
-  
+  # determine the guessing result
   if int(guess_number) == target_number: 
-    current_status()
     print(f"You got it! The answer was {guess_number}.")
+    current_status()
     break
   elif int(guess_number) > target_number:
-    print("Too high.")
+    print("Too high, guess again.")
+    current_status()
     is_first_attempt = False
   elif int(guess_number) < target_number:
-    print("Too low.")
+    print("Too low, guess again.")
+    current_status()
     is_first_attempt = False
   else: 
     print("Please input a valid number.")
+    current_status()
     is_first_attempt = False
   
 # after exit while loop is...
 # the point user run out of attempts: 
 if attempt_left <= 0 and guess_number != target_number:
-  current_status()
   print("You've run out of guesses, you lose.")
